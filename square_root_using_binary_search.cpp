ee9bdbf3378256e1f1348d038e093a761fe914bd
shyampan <101308172+shyampan@users.noreply.github.com>
2022-05-31 01:02:28 +0530

    Add files via upload

---
+++ square_root_using_binary_search.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int square_root(int n){
+    int s=0;
+    int e=n;
+    int mid=(s+e)/2;
+    int ans=-1;
+    while(s<=e){
+        int square=mid*mid;
+
+        if(square==n){
+            return mid;
+        }
+
+        else if(square<n){
+            ans=mid;
+           s=mid+1;
+         
+        }
+
+        else{
+            e=mid-1;
+
+        }
+         mid=(s+e)/2;
+    }return ans;
+}
+int main(){
+    int n;
+    cout<<"enter integer of which want to find square root";
+    cin>>n;
+   cout<<square_root(n);
+    return 0;
+}
